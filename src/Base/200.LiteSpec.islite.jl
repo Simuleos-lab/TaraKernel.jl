ea43eb41e7ce3526b2beea7ca62756d6f87f0d0e
Tarek Tohme <tarektohme1@gmail.com>
2026-01-01 03:30:20 +0200

    removed recursion-depth limit at dict level, only at flat record level

--- src/Base/200.LiteSpec.islite.jl
+++ src/Base/200.LiteSpec.islite.jl
@@ -14,18 +14,18 @@ tk_islite_literal(x) = _tk_islite_literal(x)
 
 # --- MARK: _tk_islite
 
-# --- MARK: _tk_islite: arrays-like. n is maximum recursion depth
-_tk_islite(c::AbstractVector, n::Int=1) = n == 0 ? false : all(c -> _tk_islite(c, n - 1), values(c))
-_tk_islite(c::Tuple, n::Int=1) = n == 0 ? false : all(c -> _tk_islite(c, n - 1), values(c))
+# --- MARK: _tk_islite: arrays-like
+_tk_islite(c::AbstractVector) = all(_tk_islite, values(c))
+_tk_islite(c::Tuple) = all(_tk_islite, values(c))
 
 # --- MARK: _tk_islite: dict-like
-_tk_islite(c::NamedTuple, n::Int=1) = n == 0 ? false : all(c -> _tk_islite(c, n - 1), values(c))
-_tk_islite(c::AbstractDict{String, Any}, n::Int=1) = n == 0 ? false : all(c -> _tk_islite(c, n - 1), values(c))
+_tk_islite(c::NamedTuple) = all(_tk_islite, values(c))
+_tk_islite(c::AbstractDict{String, Any}) = all(_tk_islite, values(c))
 
 # --- MARK: _tk_islite: base
-_tk_islite(x::Any, ::Int) = _tk_islite_literal(x)
+_tk_islite(x::Any) = _tk_islite_literal(x)
 
-tk_islite(x, n) = _tk_islite(x, n)
+tk_islite(x) = _tk_islite(x)
 
 # --- MARK: find_nonlite
 ## Find a non-lite value and track its path
@@ -36,74 +36,66 @@ _subpath(::AbstractVector, path, k) = string(path, "[", k, "]")
 _subpath(::Tuple, path, k) = string(path, "(", k, ")")
 _subpath(::NamedTuple, path, k) = string(path, "[", k, "]")
 
-function __find_nonlite(c, path, n)
+function __find_nonlite(c, path)
     for (k, v) in pairs(c)
         subpath = _subpath(c, path, k)
-        ret = _find_nonlite(v, subpath, n - 1)
+        ret = _find_nonlite(v, subpath)
         isnothing(ret) || return ret
     end
     return nothing
 end
 
-function _find_nonlite(x, path::AbstractString, n::Int)
-    n == 0 && return (path, x, :depth_limit)
+function _find_nonlite(x, path::AbstractString)
     _tk_islite_literal(x) && return nothing
-    return (path, x, :not_literal)
+    return (path, x)
 end
 
 _find_nonlite(
     c::AbstractVector, 
-    path::AbstractString,
-    n::Int
+    path::AbstractString
 ) = 
-    n == 0 ? (path, c, :depth_limit) : __find_nonlite(c, path, n)
+    __find_nonlite(c, path)
 
 _find_nonlite(
     c::AbstractDict{String, Any}, 
-    path::AbstractString,
-    n::Int
+    path::AbstractString
 ) = 
-    n == 0 ? (path, c, :depth_limit) : __find_nonlite(c, path, n)
+    __find_nonlite(c, path)
 
 
 # Tuples
 _find_nonlite(
     c::Tuple, 
-    path::AbstractString,
-    n::Int
+    path::AbstractString
 ) = 
-    n == 0 ? (path, c, :depth_limit) : __find_nonlite(c, path, n)
+    __find_nonlite(c, path)
 
 # NamedTuples
 _find_nonlite(
     c::NamedTuple, 
-    path::AbstractString,
-    n::Int
+    path::AbstractString
 ) =
-    n == 0 ? (path, c, :depth_limit) : __find_nonlite(c, path, n)
+    __find_nonlite(c, path)
     
 
 # 5. Fallback: scalar / leaf value
-find_nonlite(x, path::AbstractString, n::Int) = _find_nonlite(x, path, n)
+find_nonlite(x, path::AbstractString) = _find_nonlite(x, path)
 
-find_nonlite(x, n::Int) = find_nonlite(x, "obj", n)
+find_nonlite(x) = find_nonlite(x, "obj")
 
 # --- MARK: _tk_ensure_lite
-function _tk_ensure_lite(x, n::Int=1)
-    _tk_islite(x, n) && return x
+function _tk_ensure_lite(x)
+    _tk_islite(x) && return x
     
     # error branch
-    ret = find_nonlite(x, n)
+    ret = find_nonlite(x)
     @assert !isnothing(ret)
     
-    path, val, reason = ret
-    T = typeof(val)
-    
-    if reason == :depth_limit
-        msg = "_tk_ensure_lite: maximum depth ($n) reached at:\n $(path)::$(T)"
-    else
-        msg = "_tk_ensure_lite: found non-lite value at:\n $(path)::$(T)"
-    end
+    path, x = ret
+    T = typeof(x)
+    # For debugging, you can also append `, value = $(repr(x))`
+    # but that can be very verbose for big objects.
+    msg = "_tk_ensure_lite: found non-lite value at:\n $(path)::$(T)"
     throw(ArgumentError(msg))
 end
 
